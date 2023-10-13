926dcdb7c400ce53ae15dcded40d1109b273e702
Gabriel Herbert <gabriel@rei3.de>
2023-10-13 19:11:42 +0200

    UI redesign, further header collapse, loading bar

--- www/comps/header.js
+++ www/comps/header.js
@@ -11,185 +11,176 @@ export {MyHeader as default};
 
 let MyHeader = {
 	name:'my-header',
-	template:`<div class="app-header shade noPrint" :class="{ isDark:colorHeaderMainDark }">
+	template:`<div class="app-header shade noPrint" :class="{ isDark:colorHeaderMainDark }" :style="bgStyle">
 		
-		<div class="app-header-bg" :style="bgStyle" />
-		
-		<div class="app-header-content">
-			<div ref="content" class="entries">
-				
-				<!-- module hover menu action -->
-				<div class="entry no-wrap clickable" tabindex="0"
-					v-if="!showModuleIcons"
-					@click="$emit('show-module-hover-menu')"
-					@keyup.enter="$emit('show-module-hover-menu')"
-				>
-					<img src="images/dots.png" />
-				</div>
-				
-				<template v-if="!isMobile && isAdmin && !pwaSingle" >
-					<router-link class="entry no-wrap clickable" to="/builder"
-						v-if="builderEnabled"
-						:title="capGen.button.openBuilder"
-					>
-						<img src="images/builder.png" />
-					</router-link>
-					
-					<router-link class="entry no-wrap clickable" to="/admin">
-						<img src="images/serverCog.png" />
-					</router-link>
-				</template>
-				
-				<!-- home page -->
-				<router-link class="entry no-wrap clickable" to="/home" v-if="!isMobile">
-					<img src="images/home.png" />
-					<span v-if="!isMobile && pwaSingle">{{ capGen.home }}</span>
-				</router-link>
-				
-				<!-- single module link (for mobile view) -->
-				<div class="entry no-wrap clickable" tabindex="0"
-					v-if="moduleSingle !== false"
-					@click="clickSingleModuleLink"
-					@keyup.enter.space="clickSingleModuleLink"
-					:class="{ 'router-link-active':isAtMenu || moduleSingleActive }"
-				>
-					<img :src="srcBase64Icon(moduleSingle.iconId,'images/module.png')" />
-					<span>{{ moduleSingleCaption }}</span>
-				</div>
-				
-				<!-- modules -->
-				<div class="entry-wrap"
-					v-if="showModuleIcons"
-					v-for="me in moduleEntries"
-					:key="me.id"
-				>
-					<div class="entry-bg"
-						:class="{ 'router-link-active':$route.params.moduleName === me.name }"
-					/>
-					
-					<router-link class="entry clickable"
-						:class="{ 'router-link-active':$route.params.moduleName === me.name }"
-						:to="'/app/'+me.name"
-					>
-						<img :src="srcBase64Icon(me.iconId,'images/module.png')" />
-						<span v-if="settings.headerCaptions && showModuleTitles">
-							{{ me.caption }}
-						</span>
-					</router-link>
-					
-					<!-- sub header -->
-					<div class="children shade"
-						v-if="me.children.length !== 0"
-						:style="bgStyleEntries"
-					>
-						<!-- parent module (if accessible) -->
-						<router-link class="entry child clickable"
-							v-if="me.accessible"
-							:class="{ 'router-link-active':$route.params.moduleNameChild === me.name }"
-							:key="me.id"
-							:to="'/app/'+me.name+'/'+me.name"
-						>
-							<img src="images/triangleUp.png" />
-							<span>{{ me.caption }}</span>
-						</router-link>
-						
-						<!-- module children -->
-						<router-link class="entry child clickable"
-							v-for="mec in me.children"
-							:class="{ 'router-link-active':$route.params.moduleNameChild === mec.name }"
-							:key="mec.id"
-							:to="'/app/'+me.name+'/'+mec.name"
-						>
-							<img :src="srcBase64Icon(mec.iconId,'images/module.png')" />
-							<span>{{ mec.caption }}</span>
-						</router-link>
-					</div>
-				</div>
+		<div ref="content" class="entries">
+			
+			<!-- module hover menu action -->
+			<div class="entry no-wrap clickable" tabindex="0"
+				v-if="!showModuleIcons"
+				@click="$emit('show-module-hover-menu')"
+				@keyup.enter="$emit('show-module-hover-menu')"
+			>
+				<img src="images/dots.png" />
 			</div>
 			
-			<div ref="empty" class="entries" style="flex:1 1 auto;"></div>
-			
-			<div ref="system" class="entries">
-				
-				<!-- busy indicator -->
-    			<transition name="fade_out">
-					<div class="entry no-wrap"
-						v-if="busyCounter > 0"
-						:title="capGen.busy"
-					><img src="images/load.gif" /></div>
-				</transition>
-				
-				<!-- collection entries -->
-				<div class="entry no-wrap" tabindex="0"
-					v-if="showCollections"
-					v-for="e in collectionEntries"
-					@click="formOpen(e.openForm)"
-					@keyup.enter="formOpen(e.openForm)"
-					:class="{ clickable:e.openForm !== null, readonly:e.openForm === null }"
-					:title="e.title"
+			<template v-if="!isMobile && isAdmin && !pwaSingle" >
+				<router-link class="entry no-wrap clickable" to="/builder"
+					v-if="builderEnabled"
+					:title="capGen.button.openBuilder"
 				>
-					<img v-if="e.iconId !== null" :src="srcBase64Icon(e.iconId,'')" />
-					<span>{{ e.value }}</span>
-				</div>
-				<div class="entry clickable" tabindex="0"
-					v-if="collectionCounter !== 0"
-					@click="$emit('show-collection-input',collectionEntries)"
-					@keyup.enter="$emit('show-collection-input',collectionEntries)"
-				>
-					<img src="images/bell.png" />
-					<span>{{ collectionCounter }}</span>
-				</div>
-				
-				<!-- navigation -->
-				<div class="entry no-wrap clickable" tabindex="0"
-					@click="pagePrev"
-					@keyup.enter="pagePrev"
-				>
-					<img src="images/pagePrev.png" />
-				</div>
-				<div class="entry no-wrap clickable" tabindex="0"
-					v-if="!isMobile"
-					@click="pageNext"
-					@keyup.enter="pageNext"
-				>
-					<img src="images/pageNext.png" />
-				</div>
-				
-				<!-- keys locked -->
-				<div class="entry no-wrap clickable" tabindex="0"
-					v-if="keysLocked"
-					@click="keysLockedMsg"
-					@keyup.enter="keysLockedMsg"
-				>
-					<img src="images/key_locked.png" />
-				</div>
-				
-				<!-- feedback -->
-				<div class="entry no-wrap clickable" tabindex="0"
-					v-if="feedback && !isNoAuth"
-					@click="openFeedback"
-					@keyup.enter="openFeedback"
-				>
-					<img src="images/feedback.png" />
-				</div>
-				
-				<!-- settings -->
-				<router-link class="entry no-wrap clickable" to="/settings"
-					v-if="!isNoAuth"
-				>
-					<img src="images/person.png" />
+					<img src="images/builder.png" />
 				</router-link>
 				
-				<!-- log off -->
-				<div class="entry no-wrap clickable" tabindex="0"
-					v-if="isNoAuth"
-					@click="$emit('logout')"
-					@keyup.enter="$emit('logout')"
+				<router-link class="entry no-wrap clickable" to="/admin">
+					<img src="images/serverCog.png" />
+				</router-link>
+			</template>
+			
+			<!-- home page -->
+			<router-link class="entry no-wrap clickable" to="/home" v-if="!isMobile">
+				<img src="images/home.png" />
+				<span v-if="!isMobile && pwaSingle">{{ capGen.home }}</span>
+			</router-link>
+			
+			<!-- single module link (for mobile view) -->
+			<div class="entry no-wrap clickable" tabindex="0"
+				v-if="moduleSingle !== false"
+				@click="clickSingleModuleLink"
+				@keyup.enter.space="clickSingleModuleLink"
+				:class="{ 'router-link-active':isAtMenu || moduleSingleActive }"
+			>
+				<img :src="srcBase64Icon(moduleSingle.iconId,'images/module.png')" />
+				<span>{{ moduleSingleCaption }}</span>
+			</div>
+			
+			<!-- modules -->
+			<div class="entry-wrap"
+				v-if="showModuleIcons"
+				v-for="me in moduleEntries"
+				:key="me.id"
+			>
+				<div class="entry-bg"
+					:class="{ 'router-link-active':$route.params.moduleName === me.name }"
+				/>
+				
+				<router-link class="entry clickable"
+					:class="{ 'router-link-active':$route.params.moduleName === me.name }"
+					:to="'/app/'+me.name"
 				>
-					<img src="images/logoff.png" />
+					<img :src="srcBase64Icon(me.iconId,'images/module.png')" />
+					<span v-if="settings.headerCaptions && showModuleTitles">
+						{{ me.caption }}
+					</span>
+				</router-link>
+				
+				<!-- sub header -->
+				<div class="children shade"
+					v-if="me.children.length !== 0"
+					:style="bgStyleEntries"
+				>
+					<!-- parent module (if accessible) -->
+					<router-link class="entry child clickable"
+						v-if="me.accessible"
+						:class="{ 'router-link-active':$route.params.moduleNameChild === me.name }"
+						:key="me.id"
+						:to="'/app/'+me.name+'/'+me.name"
+					>
+						<img src="images/triangleUp.png" />
+						<span>{{ me.caption }}</span>
+					</router-link>
+					
+					<!-- module children -->
+					<router-link class="entry child clickable"
+						v-for="mec in me.children"
+						:class="{ 'router-link-active':$route.params.moduleNameChild === mec.name }"
+						:key="mec.id"
+						:to="'/app/'+me.name+'/'+mec.name"
+					>
+						<img :src="srcBase64Icon(mec.iconId,'images/module.png')" />
+						<span>{{ mec.caption }}</span>
+					</router-link>
 				</div>
 			</div>
 		</div>
+		
+		<div ref="empty" class="entries empty"></div>
+		
+		<div ref="system" class="entries">
+			
+			<!-- collection entries -->
+			<div class="entry no-wrap" tabindex="0"
+				v-if="showCollections"
+				v-for="e in collectionEntries"
+				@click="formOpen(e.openForm)"
+				@keyup.enter="formOpen(e.openForm)"
+				:class="{ clickable:e.openForm !== null, readonly:e.openForm === null }"
+				:title="e.title"
+			>
+				<img v-if="e.iconId !== null" :src="srcBase64Icon(e.iconId,'')" />
+				<span>{{ e.value }}</span>
+			</div>
+			<div class="entry clickable" tabindex="0"
+				v-if="collectionCounter !== 0"
+				@click="$emit('show-collection-input',collectionEntries)"
+				@keyup.enter="$emit('show-collection-input',collectionEntries)"
+			>
+				<img src="images/bell.png" />
+				<span>{{ collectionCounter }}</span>
+			</div>
+			
+			<!-- navigation -->
+			<div class="entry no-wrap clickable" tabindex="0"
+				v-if="showNavPrev"
+				@click="pagePrev"
+				@keyup.enter="pagePrev"
+			>
+				<img src="images/pagePrev.png" />
+			</div>
+			<div class="entry no-wrap clickable" tabindex="0"
+				v-if="showNavNext"
+				@click="pageNext"
+				@keyup.enter="pageNext"
+			>
+				<img src="images/pageNext.png" />
+			</div>
+			
+			<!-- keys locked -->
+			<div class="entry no-wrap clickable" tabindex="0"
+				v-if="keysLocked"
+				@click="keysLockedMsg"
+				@keyup.enter="keysLockedMsg"
+			>
+				<img src="images/key_locked.png" />
+			</div>
+			
+			<!-- feedback -->
+			<div class="entry no-wrap clickable" tabindex="0"
+				v-if="showFeedback"
+				@click="openFeedback"
+				@keyup.enter="openFeedback"
+			>
+				<img src="images/feedback.png" />
+			</div>
+			
+			<!-- settings -->
+			<router-link class="entry no-wrap clickable" to="/settings"
+				v-if="!isNoAuth"
+			>
+				<img src="images/person.png" />
+			</router-link>
+			
+			<!-- log off -->
+			<div class="entry no-wrap clickable" tabindex="0"
+				v-if="isNoAuth"
+				@click="$emit('logout')"
+				@keyup.enter="$emit('logout')"
+			>
+				<img src="images/logoff.png" />
+			</div>
+		</div>
+		
+		<div class="app-header-loading" v-if="busyCounter > 0"></div>
 	</div>`,
 	props:{
 		keysLocked:   { type:Boolean, required:true },
@@ -198,16 +189,15 @@ let MyHeader = {
 	emits:['logout','show-collection-input','show-module-hover-menu'],
 	data() {
 		return {
-			contentSizePx:0,        // width in pixel required by all application entries
-			contentSizePxBuffer:50, // keep this space free (currently only for busy indicator)
-			
-			// layout adjustment
 			layoutCheckTimer:null,
 			layoutReducedElements:[],        // list of elements that needed to be reduced to fit the current window width
 			layoutReducibleElementsInOrder:[ // list of elements that can be reduced, in order of priority
-				'moduleTitles', // module titles, take up the most space, removed fully
-				'collections',  // collection values/icons, replaced by hover menu
-				'moduleIcons'   // module icons, replaced by hover menu
+				'moduleTitles',   // module titles, take up the most space, removed fully
+				'collections',    // collection values/icons, replaced by hover menu
+				'moduleIcons',    // module icons, replaced by hover menu
+				'navigationNext', // browser forward navigation
+				'navigationPrev', // browser previous navigation
+				'feedback'        // feedback action
 			],
 		};
 	},
@@ -309,8 +299,11 @@ let MyHeader = {
 		// simple
 		pwaSingle:       (s) => s.pwaModuleId !== null,
 		showCollections: (s) => !s.elementsReduced.includes('collections'),
+		showFeedback:    (s) => !s.elementsReduced.includes('feedback') && s.feedback && !s.isNoAuth,
 		showModuleIcons: (s) => !s.elementsReduced.includes('moduleIcons'),
 		showModuleTitles:(s) => !s.elementsReduced.includes('moduleTitles'),
+		showNavNext:     (s) => !s.elementsReduced.includes('navigationNext'),
+		showNavPrev:     (s) => !s.elementsReduced.includes('navigationPrev'),
 		
 		// stores
 		modules:            (s) => s.$store.getters['schema/modules'],
@@ -356,7 +349,7 @@ let MyHeader = {
 		// display
 		layoutAdjust() {
 			this.layoutCheckTimer = null;
-			const enoughSpace = this.$refs.empty.offsetWidth > this.contentSizePxBuffer;
+			const enoughSpace = this.$refs.empty.offsetWidth > 10;
 			
 			if(enoughSpace || this.elementsReduced.length === this.layoutReducibleElementsInOrder.length)
 				return;
