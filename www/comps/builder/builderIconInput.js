cf3eebff4b49d591d8d4e8d5b3b58cd47530909f
Gabriel Herbert <gabriel@rei3.de>
2023-11-14 10:54:26 +0100

    Small UI fixes

--- www/comps/builder/builderIconInput.js
+++ www/comps/builder/builderIconInput.js
@@ -34,7 +34,7 @@ let MyBuilderIconInput = {
 		
 		<div class="app-sub-window under-header" v-if="showInput && iconIdMap !== null" @click.self="close">
 			<div class="build-icon-input-window">
-				<div class="contentBox popUp">
+				<div class="contentBox float">
 					<div class="top lower">
 						<div class="area">
 							<h1 class="title">{{ capGen.icon }}</h1>
