6765ee3df0c2e3cf17589a8be77ca38d26caf151
Gabriel Herbert <gabriel@rei3.de>
2023-12-11 12:11:25 +0100

    Fixed: Backup edge cases
    * Fixed: Backups can fail if a symlink is created inside files directory on Linux systems.
    * Fixed: Database backup directory has different permissions to other files.
    * Fixed: Files backup zip file can miss directories on Windows systems.

--- tools/compress/compress.go
+++ tools/compress/compress.go
@@ -19,51 +19,36 @@ func Path(zipPath string, sourcePath string) error {
 	zipWriter := zip.NewWriter(zipFile)
 	defer zipWriter.Close()
 
-	sourcePathInfo, err := os.Stat(sourcePath)
-	if err != nil {
-		return err
-	}
-
-	var baseDir string
-	if sourcePathInfo.IsDir() {
-		baseDir = filepath.Base(sourcePath)
-	}
-
-	filepath.Walk(sourcePath, func(path string, info os.FileInfo, err error) error {
+	return filepath.Walk(sourcePath, func(pathWalked string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		// ignore directories themselves
-		// included files have header paths which include their respective paths
+		// directories do not need to be created in zip files
 		if info.IsDir() {
 			return nil
 		}
 
-		header, err := zip.FileInfoHeader(info)
+		// ignore non-regular files (symbolic links, devices, named pipes, sockets, ...)
+		if !info.Mode().IsRegular() {
+			return nil
+		}
+
+		fileWalked, err := os.Open(pathWalked)
+		if err != nil {
+			return err
+		}
+		defer fileWalked.Close()
+
+		// trim source directory from file path
+		pathWalkedRel := strings.TrimPrefix(pathWalked, filepath.Dir(sourcePath)+string(os.PathSeparator))
+
+		zipFileWriter, err := zipWriter.Create(pathWalkedRel)
 		if err != nil {
 			return err
 		}
 
-		if baseDir != "" {
-			// trim prefix to remove source path from file path inside zip
-			header.Name = strings.Trim(strings.TrimPrefix(path, filepath.Clean(sourcePath)), "/\\")
-		}
-		header.Method = zip.Deflate
-
-		writer, err := zipWriter.CreateHeader(header)
-		if err != nil {
-			return err
-		}
-
-		file, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-
-		_, err = io.Copy(writer, file)
+		_, err = io.Copy(zipFileWriter, fileWalked)
 		return err
 	})
-	return nil
 }
