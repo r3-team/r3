a3112b7a9108b777ad01abfafa7e480786af5a46
Gabriel Herbert <gabriel@rei3.de>
2024-01-16 09:16:45 +0100

    Latvian community translation

--- cache/cache_caption.go
+++ cache/cache_caption.go
@@ -1,5 +1,5 @@
 package cache
 
 func GetCaptionLanguageCodes() []string {
-	return []string{"en_us", "de_de", "fr_fr", "hu_hu", "it_it", "ro_ro"}
+	return []string{"en_us", "de_de", "fr_fr", "hu_hu", "it_it", "lv_lv", "ro_ro"}
 }
