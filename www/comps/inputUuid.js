66b74b17ac83ef8f1fad2bc86af6bd6b68b28e1a
Gabriel Herbert <gabriel@rei3.de>
2025-04-29 11:53:14 +0200

    Reworked UUID input field
    * Added: Clear input action.
    * Tabindex will now only be set for filled in UUID inputs and the ones directly following them.
    * Field focus will now jump to the next input field when the previous one was filled completely.
    * Fix: Tabbing into field without entering anything shows field value as invalid.
    * Fix: Removing all values will not reset field value to null.

--- www/comps/inputUuid.js
+++ www/comps/inputUuid.js
@@ -3,15 +3,22 @@ export {MyInputUuid as default};
 let MyInputUuid = {
 	name:'my-input-uuid',
 	template:`<div class="input-uuid">
-		<input v-model="val[0]" :disabled="readonly" :placeholder="getHolder(8)"  size="8" @keyup="setFirstBlock($event.target.value)" data-is-input="1" />
-		<span>-</span>
-		<input v-model="val[1]" :disabled="readonly" :placeholder="getHolder(4)"  size="4"  maxlength="4"  @keyup="set" />
-		<span>-</span>
-		<input v-model="val[2]" :disabled="readonly" :placeholder="getHolder(4)"  size="4"  maxlength="4"  @keyup="set" />
-		<span>-</span>
-		<input v-model="val[3]" :disabled="readonly" :placeholder="getHolder(4)"  size="4"  maxlength="4"  @keyup="set" />
-		<span>-</span>
-		<input v-model="val[4]" :disabled="readonly" :placeholder="getHolder(12)" size="12" maxlength="12" @keyup="set" />
+		<div class="row wrap">
+			<input v-model="val[0]" @keyup="setValue(0)" :disabled="readonly" :placeholder="getHolder(lengths[0])"  maxlength="36"         :size="lengths[0]" ref="input0" data-is-input="1" />
+			<span>-</span>
+			<input v-model="val[1]" @keyup="setValue(1)" :disabled="readonly" :placeholder="getHolder(lengths[1])" :maxlength="lengths[1]" :size="lengths[1]" ref="input1" :tabindex="getTabindex(1)" />
+			<span>-</span>
+			<input v-model="val[2]" @keyup="setValue(2)" :disabled="readonly" :placeholder="getHolder(lengths[2])" :maxlength="lengths[2]" :size="lengths[2]" ref="input2" :tabindex="getTabindex(2)" />
+			<span>-</span>
+			<input v-model="val[3]" @keyup="setValue(3)" :disabled="readonly" :placeholder="getHolder(lengths[3])" :maxlength="lengths[3]" :size="lengths[3]" ref="input3" :tabindex="getTabindex(3)" />
+			<span>-</span>
+			<input v-model="val[4]" @keyup="setValue(4)" :disabled="readonly" :placeholder="getHolder(lengths[4])" :maxlength="lengths[4]" :size="lengths[4]" ref="input4" :tabindex="getTabindex(4)" />
+		</div>
+		<my-button image="cancel.png"
+			@trigger="clear"
+			:active="!isEmpty && !readonly"
+			:naked="true"
+		/>
 	</div>`,
 	props:{
 		modelValue:{ required:true },
@@ -19,10 +26,14 @@ let MyInputUuid = {
 	},
 	data() {
 		return {
+			lengths:[8,4,4,4,12],
 			val:['','','','','']
 		};
 	},
 	emits:['update:modelValue'],
+	computed:{
+		isEmpty:(s) => s.val[0] === '' && s.val[1] === '' && s.val[2] === '' && s.val[3] === '' && s.val[4] === '',
+	},
 	watch:{
 		modelValue:{
 			handler(valNew,valOld) {
@@ -37,22 +48,49 @@ let MyInputUuid = {
 		}
 	},
 	methods:{
+		clear() {
+			this.reset();
+			this.setValueCombined();
+			this.setActiveInput(0);
+		},
 		getHolder(n) {
 			return '.'.repeat(n);
+		},
+		getTabindex(i) {
+			return this.val[i-1] === '' && this.val[i] === '' ? -1 : 0;
 		},
 		reset() {
 			this.val = ['','','','',''];
 		},
-		set() {
-			if(this.val.join('-') !== this.modelValue)
-				this.$emit('update:modelValue',this.val.join('-'));
+		setActiveInput(i) {
+			const el = this.$refs[`input${i}`];
+			if(el !== undefined) {
+				el.focus();
+				this.$nextTick(() => el.select());
+			}
 		},
-		setFirstBlock(v) {
-			if(v.length === 36 && v.split('-').length === 5)
-				return this.$emit('update:modelValue',v);
-			
-			this.val[0] = v.substring(0,8);
-			this.set();
+		setValue(i) {
+			// special case: a full UUID was entered in first input field
+			if(i === 0 && this.val[0].length === 36 && this.val[0].split('-').length === 5) {
+				this.setActiveInput(0);
+				return this.$emit('update:modelValue',this.val[0]);
+			}
+
+			// shorten input values to max. length
+			if(this.val[i].length > this.lengths[i])
+				this.val[i] = this.val[i].substring(0,8);
+
+			// send new value
+			this.setValueCombined();
+
+			// jump to next input if current input is full and not last one
+			if(this.val[i].length === this.lengths[i] && i < this.val.length - 1)
+				this.setActiveInput(i+1);
+		},
+		setValueCombined() {
+			const v = this.isEmpty ? null : this.val.join('-');
+			if(v !== this.modelValue)
+				this.$emit('update:modelValue',v);
 		}
 	}
 };
