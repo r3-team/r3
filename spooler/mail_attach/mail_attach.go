76a51fce7b3b4b2cdeacd36dde03e11d85147e8d
Gabriel Herbert <gabriel@rei3.de>
2025-09-14 22:50:52 +0200

    Better error handling for mail attach job
    Non-files attribute & non-existing record errors will now still clear the mail spooler.

--- spooler/mail_attach/mail_attach.go
+++ spooler/mail_attach/mail_attach.go
@@ -9,11 +9,13 @@ import (
 	"r3/cache"
 	"r3/data"
 	"r3/db"
+	"r3/log"
 	"r3/schema"
 	"r3/tools"
 	"r3/types"
 
 	"github.com/gofrs/uuid"
+	"github.com/jackc/pgx/v5"
 )
 
 func DoAll() error {
@@ -50,23 +52,29 @@ func DoAll() error {
 
 func do(mail types.Mail) error {
 	cache.Schema_mx.RLock()
-	defer cache.Schema_mx.RUnlock()
-
-	// check validity of record and attributes to attach files to
 	atr, exists := cache.AttributeIdMap[mail.AttributeId.Bytes]
-	if !exists {
-		return fmt.Errorf("cannot attach file(s) to unknown attribute %s", mail.AttributeId.String())
+	var rel types.Relation
+	var mod types.Module
+	if exists {
+		rel = cache.RelationIdMap[atr.RelationId]
+		mod = cache.ModuleIdMap[rel.ModuleId]
 	}
+	cache.Schema_mx.RUnlock()
 
+	ctx, ctxCanc := context.WithTimeout(context.Background(), db.CtxDefTimeoutSysTask)
+	defer ctxCanc()
+
+	// check validity of attribute to attach files to
+	if !exists {
+		return fmt.Errorf("cannot attach file(s) to unknown attribute '%s'", mail.AttributeId.String())
+	}
 	if !schema.IsContentFiles(atr.Content) {
-		return fmt.Errorf("cannot attach file(s) to non-file attribute %s", mail.AttributeId.String())
+		log.Error(log.ContextMail, fmt.Sprintf("cannot store attachments in non-files attribute '%s'", atr.Name), fmt.Errorf("deleting mail"))
+		return deleteMail(ctx, mail.Id)
 	}
 
 	// get files from spooler
-	fileIds := make([]uuid.UUID, 0)
-	filesMail := make([]types.MailFile, 0)
-
-	rows, err := db.Pool.Query(context.Background(), `
+	rows, err := db.Pool.Query(ctx, `
 		SELECT file, file_name, file_size
 		FROM instance.mail_spool_file
 		WHERE mail_id = $1
@@ -76,6 +84,7 @@ func do(mail types.Mail) error {
 	}
 	defer rows.Close()
 
+	filesMail := make([]types.MailFile, 0)
 	for rows.Next() {
 		var f types.MailFile
 
@@ -83,20 +92,28 @@ func do(mail types.Mail) error {
 		if err != nil {
 			return err
 		}
-
 		if err := rows.Scan(&f.File, &f.Name, &f.Size); err != nil {
 			return err
 		}
-		fileIds = append(fileIds, f.Id)
 		filesMail = append(filesMail, f)
 	}
+	rows.Close()
 
-	// no attachments to process, just delete mail
 	if len(filesMail) == 0 {
-		_, err = db.Pool.Exec(context.Background(), `
-			DELETE FROM instance.mail_spool
-			WHERE id = $1
-		`, mail.Id)
+		// no attachments to process, delete mail
+		return deleteMail(ctx, mail.Id)
+	}
+
+	if err := db.Pool.QueryRow(ctx, fmt.Sprintf(`
+		SELECT TRUE
+		FROM %s.%s
+		WHERE %s = $1
+	`, mod.Name, rel.Name, schema.PkName), mail.RecordId.Int64).Scan(&exists); err != nil {
+		if err == pgx.ErrNoRows {
+			// record does not exist, delete mail
+			log.Warning(log.ContextMail, fmt.Sprintf("cannot store attachments for record ID %d, record does not exist", mail.RecordId.Int64), fmt.Errorf("deleting mail"))
+			return deleteMail(ctx, mail.Id)
+		}
 		return err
 	}
 
@@ -125,9 +142,6 @@ func do(mail types.Mail) error {
 
 	// store file changes
 	// update the database only after all files have physically been saved
-	ctx, ctxCanc := context.WithTimeout(context.Background(), db.CtxDefTimeoutSysTask)
-	defer ctxCanc()
-
 	tx, err := db.Pool.Begin(ctx)
 	if err != nil {
 		return err
@@ -135,7 +149,6 @@ func do(mail types.Mail) error {
 	defer tx.Rollback(ctx)
 
 	fileIdMapChange := make(map[uuid.UUID]types.DataSetFileChange)
-	rel, _ := cache.RelationIdMap[atr.RelationId]
 	for _, f := range filesMail {
 		if err := data.FileApplyVersion_tx(ctx, tx, true, atr.Id, rel.Id,
 			f.Id, f.Hash, f.Name, f.Size, 0, []int64{mail.RecordId.Int64}, -1); err != nil {
@@ -152,13 +165,26 @@ func do(mail types.Mail) error {
 	if err := data.FilesApplyAttributChanges_tx(ctx, tx, mail.RecordId.Int64, atr.Id, fileIdMapChange); err != nil {
 		return err
 	}
-
-	// all done, delete mail
-	if _, err := tx.Exec(ctx, `
-		DELETE FROM instance.mail_spool
-		WHERE id = $1
-	`, mail.Id); err != nil {
+	if err := deleteMail_tx(ctx, tx, mail.Id); err != nil {
 		return err
 	}
 	return tx.Commit(ctx)
+}
+
+// helpers
+func deleteMail(ctx context.Context, id int64) error {
+	tx, err := db.Pool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback(ctx)
+
+	if err := deleteMail_tx(ctx, tx, id); err != nil {
+		return err
+	}
+	return tx.Commit(ctx)
+}
+func deleteMail_tx(ctx context.Context, tx pgx.Tx, id int64) error {
+	_, err := tx.Exec(ctx, `DELETE FROM instance.mail_spool WHERE id = $1`, id)
+	return err
 }
