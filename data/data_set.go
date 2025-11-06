r3-team/r3

commit 02fd5648c01ddcd4aaf5e1833aba26a32714ef8f

Fix: Write perms for relation not checked for existing records

data/data_set.go
@@ -55,7 +55,7 @@ func Set_tx(ctx context.Context, tx pgx.Tx, dataSetsByIndex map[int]types.DataSe
 			return indexRecordIds, handler.ErrSchemaUnknownRelation(dataSet.RelationId)
 		}
 
-		if isNewRecord && !authorizedRelation(loginId, dataSet.RelationId, types.AccessWrite) {
+		if !authorizedRelation(loginId, dataSet.RelationId, types.AccessWrite) {
 			return indexRecordIds, errors.New(handler.ErrUnauthorized)
 		}
 

commit 71147e32aff5207f424f2e047fa6d8f5928f0f22

Fix: Cannot create joined record if base relation has no write permission
Regression from 02fd5648c01ddcd4aaf5e1833aba26a32714ef8f: While it is important to check write access to relation if SET is requested, access can be ignored if no values are to be updated on this relation.

Case: Base record exists on relation with readonly permission, but joined record should be created. Base relation is still included in SET as it´s required to create the joined record and update it´s relationship. This is only relevant in 1:n cases. If the relationship attribute is located on the base relation, write permissions are required on it.

data/data_set.go
@@ -55,7 +55,10 @@ func Set_tx(ctx context.Context, tx pgx.Tx, dataSetsByIndex map[int]types.DataSe
 			return indexRecordIds, handler.ErrSchemaUnknownRelation(dataSet.RelationId)
 		}
 
-		if !authorizedRelation(loginId, dataSet.RelationId, types.AccessWrite) {
+		// check write access to relation
+		// if no attributes are to be SET for an existing record, WRITE permission is not required
+		//  case: joined record is to be created but existing base record is untouched, SET still includes base relation (to resolve relationship)
+		if (isNewRecord || len(dataSet.Attributes) != 0) && !authorizedRelation(loginId, dataSet.RelationId, types.AccessWrite) {
 			return indexRecordIds, errors.New(handler.ErrUnauthorized)
 		}
 