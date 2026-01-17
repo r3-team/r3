673c38fac674d65a017648755cc347bec2936213
Gabriel Herbert <gabriel@rei3.de>
2026-01-17 13:02:32 +0100

    Fix: Grid field does not shrink correctly

--- spooler/doc_create/doc_create_field_grid.go
+++ spooler/doc_create/doc_create_field_grid.go
@@ -9,14 +9,20 @@ import (
 
 func addFieldGrid(ctx context.Context, doc *doc, f types.DocFieldGrid, font types.DocFont, posX, posY, pageYUsable, pageMarginT float64) (float64, error) {
 
+	// get border sizes
+	_, bSizeT, bSizeR, bSizeB, bSizeL := getBorderSize(f.Border)
+	bSizeX := bSizeL + bSizeR
+	bSizeY := bSizeT + bSizeB
+
 	// grid fields can never be higher than the usable page height
 	if f.SizeY > pageYUsable {
 		return posY, nil
 	}
 
-	_, bOffsetT, bOffsetR, bOffsetB, bOffsetL := getBorderSize(f.Border)
-	posXChildren := posX + bOffsetL
-	posYChildren := posY + bOffsetT
+	// field size is defined space including borders, borders reduce available space for children
+	// place children inside
+	posXChildren := posX + bSizeL
+	posYChildren := posY + bSizeT
 
 	var posYChildMax float64
 	for _, fieldIfChild := range f.Fields {
@@ -29,16 +35,20 @@ func addFieldGrid(ctx context.Context, doc *doc, f types.DocFieldGrid, font type
 			posYChildMax = posYAfterFields
 		}
 	}
-	if posYChildMax > posY+f.SizeY || !f.Shrink {
-		// exceeding grid field height is not allowed
-		// not reaching its height is allowed, if field shrink is enabled
+	childrenExceedParent := posYChildMax > f.SizeY-bSizeB
+
+	if f.Shrink && !childrenExceedParent {
+		// can shrink and there is place free, stay where we are and add space for bottom border
+		posYChildMax += bSizeB
+	} else {
+		// cannot shrink or children exceed parent, set to fixed grid size incl. border
 		posYChildMax = posY + f.SizeY
 	}
 
 	// draw layout container from its start position up to its calculated height
 	// border offsets are halved as border lines are drawn over lines (half going over, half under)
-	doc.p.SetXY(posX+(bOffsetL/2), posY+(bOffsetT/2))
-	drawBox(doc, f.Border, pgtype.Text{}, f.SizeX-((bOffsetL/2)+(bOffsetR/2)), posYChildMax-posY-((bOffsetT/2)+(bOffsetB/2)))
+	doc.p.SetXY(posX+(bSizeL/2), posY+(bSizeT/2))
+	drawBox(doc, f.Border, pgtype.Text{}, f.SizeX-(bSizeX/2), posYChildMax-posY-(bSizeY/2))
 
 	return posYChildMax, nil
 }
