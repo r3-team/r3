r3-team/r3

commit 4120fd5f84085173ccd4e747d9e1c20351338c81

Optimized attribute access checks

data/data.go
@@ -15,30 +15,36 @@ import (
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
-// check whether access to attribute is authorized
-// cases: getting or setting attribute values
-func authorizedAttribute(loginId int64, attributeId uuid.UUID, accessRequested types.Access) bool {
+// check whether access (DELETE, READ, WRITE) to attributes is authorized
+// returns false, if any attribute access is prohibited
+func authorizedAttributes(loginId int64, attributeIds []uuid.UUID, accessRequested types.Access) bool {
 
 	m, err := cache.GetAccessById(loginId)
 	if err != nil {
 		return false
 	}
 
-	// use attribute access first if specified (more specific access wins)
-	if access, exists := m.Attribute[attributeId]; exists {
-		return access >= accessRequested
-	}
+	for _, attributeId := range attributeIds {
 
-	// use relation access otherwise (inherited access)
-	atr, exists := cache.AttributeIdMap[attributeId]
-	if !exists {
+		// use attribute access first if specified (more specific access wins)
+		if access, exists := m.Attribute[attributeId]; exists && access >= accessRequested {
+			continue
+		}
+
+		// use relation access otherwise (inherited access)
+		atr, exists := cache.AttributeIdMap[attributeId]
+		if !exists {
+			return false
+		}
+
+		if access, exists := m.Relation[atr.RelationId]; exists && access >= accessRequested {
+			continue
+		}
+
+		// no access to attribute or relation
 		return false
 	}
-
-	if access, exists := m.Relation[atr.RelationId]; exists {
-		return access >= accessRequested
-	}
-	return false
+	return true
 }
 
 // check whether access to relation is authorized

data/data_file.go
@@ -42,7 +42,7 @@ func MayAccessFile(loginId int64, attributeId uuid.UUID) error {
 		return errors.New("not a file attribute")
 	}
 
-	if !authorizedAttribute(loginId, attributeId, types.AccessRead) {
+	if !authorizedAttributes(loginId, []uuid.UUID{attributeId}, types.AccessRead) {
 		return errors.New(handler.ErrUnauthorized)
 	}
 	return nil
@@ -74,7 +74,7 @@ func SetFile(ctx context.Context, loginId int64, attributeId, fileId uuid.UUID, 
 	}
 
 	// check for access permissions, unless it´s a system task (login ID = -1)
-	if loginId != -1 && !authorizedAttribute(loginId, attributeId, types.AccessWrite) {
+	if loginId != -1 && !authorizedAttributes(loginId, []uuid.UUID{attributeId}, types.AccessWrite) {
 		return errors.New(handler.ErrUnauthorized)
 	}
 

data/data_get.go
@@ -277,10 +277,14 @@ func prepareQuery(data types.DataGet, indexRelationIds map[int]uuid.UUID, queryA
 
 	// check for access permissions, unless it´s a system task (login ID = -1)
 	if loginId != -1 {
+		attributeIdsReadAccess := make([]uuid.UUID, 0)
 		for _, expr := range data.Expressions {
-			if expr.AttributeId.Valid && !authorizedAttribute(loginId, expr.AttributeId.Bytes, types.AccessRead) {
-				return "", errors.New(handler.ErrUnauthorized)
+			if expr.AttributeId.Valid {
+				attributeIdsReadAccess = append(attributeIdsReadAccess, expr.AttributeId.Bytes)
 			}
+		}
+		if !authorizedAttributes(loginId, attributeIdsReadAccess, types.AccessRead) {
+			return "", errors.New(handler.ErrUnauthorized)
 		}
 	}
 

data/data_set.go
@@ -61,16 +61,12 @@ func Set_tx(ctx context.Context, tx pgx.Tx, dataSetsByIndex map[int]types.DataSe
 			return indexRecordIds, errors.New(handler.ErrUnauthorized)
 		}
 
-		// check write access for updating attribute values
+		// check write access for updating attribute values & check for protected preset record values
+		attributeIdsWriteAccess := make([]uuid.UUID, len(dataSet.Attributes))
 		for _, attribute := range dataSet.Attributes {
+			attributeIdsWriteAccess = append(attributeIdsWriteAccess, attribute.AttributeId)
 
-			if !authorizedAttribute(loginId, attribute.AttributeId, types.AccessWrite) {
-				return indexRecordIds, errors.New(handler.ErrUnauthorized)
-			}
-
-			// check for protected preset record values
 			for _, preset := range rel.Presets {
-
 				if cache.GetPresetRecordId(preset.Id) != dataSet.RecordId {
 					continue
 				}
@@ -82,12 +78,13 @@ func Set_tx(ctx context.Context, tx pgx.Tx, dataSetsByIndex map[int]types.DataSe
 						if !exists {
 							return indexRecordIds, handler.ErrSchemaUnknownAttribute(attribute.AttributeId)
 						}
-
-						return indexRecordIds, fmt.Errorf("cannot change attribute value '%s' of protected preset '%s'",
-							atr.Name, preset.Name)
+						return indexRecordIds, fmt.Errorf("cannot change attribute value '%s' of protected preset '%s'", atr.Name, preset.Name)
 					}
 				}
 			}
+		}
+		if !authorizedAttributes(loginId, attributeIdsWriteAccess, types.AccessWrite) {
+			return indexRecordIds, errors.New(handler.ErrUnauthorized)
 		}
 
 		// set data for record of given relation index

commit 1e897b39f19b862a1a20b046c9c93bb711a8932b

fix regression in authorizedAttributes
4120fd5f84085173ccd4e747d9e1c20351338c81 accidentally granted access to attributes with individual access rights

data/data.go
@@ -28,7 +28,12 @@ func authorizedAttributes(loginId int64, attributeIds []uuid.UUID, accessRequest
 	for _, attributeId := range attributeIds {
 
 		// use attribute access first if specified (more specific access wins)
-		if access, exists := m.Attribute[attributeId]; exists && access >= accessRequested {
+		access, exists := m.Attribute[attributeId];
+		if exists && access < accessRequested {
+			return false
+		}
+
+		if exists && access >= accessRequested {
 			continue
 		}
 