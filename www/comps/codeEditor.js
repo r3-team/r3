20b7c5e9ea4e48ad1203dc704201bae24b8cdeae
Gabriel Herbert <gabriel@rei3.de>
2026-08-07 15:23:20 +0200

    Lazy load ACE code editor

--- www/comps/codeEditor.js
+++ www/comps/codeEditor.js
@@ -1,12 +1,10 @@
-import {
-	builderOptionGet,
-	builderOptionSet
-} from './shared/builder.js';
+import { builderOptionGet, builderOptionSet } from './shared/builder.js';
+import { jsLibraryLoadNoCache } from './shared/jsLibrary.js';
 
 export default {
-	name:'my-code-editor',
-	template:`<div class="code-editor">
-	<div class="code-editor-field" ref="codeEditor"></div>
+	name: 'my-code-editor',
+	template: `<div class="code-editor">
+		<div class="code-editor-field" ref="codeEditor"></div>
 		<div class="code-editor-options default-inputs">
 			<span><i>{{ mode }}</i></span>
 			<div class="row gap centered">
@@ -27,39 +25,42 @@ export default {
 			</div>
 		</div>
 	</div>`,
-	props:{
-		insertEntity: { required:false, default:null },
-		mode:         { type:String,  required:true },
-		modelValue:   { required:true },
-		modelValueAlt:{ type:String,  required:false, default:'' },
-		readonly:     { type:Boolean, required:false, default:false }
+	props: {
+		insertEntity: { required: false, default: null },
+		mode: { type: String, required: true },
+		modelValue: { required: true },
+		modelValueAlt: { type: String, required: false, default: '' },
+		readonly: { type: Boolean, required: false, default: false }
 	},
 	data() {
 		return {
-			editor:null,
-			printMargin:true,
-			sessionLive:'',
-			sessionPreview:'',
-			theme:'monokai',
-			themesBright:['chrome','cloud9_day','eclipse','github','textmate'],
-			themesDark:[
-				'chaos','cloud9_night','github_dark','gruvbox','gruvbox_dark_hard',
-				'kr_theme','merbivore','mono_industrial','monokai','terminal',
-				'tomorrow_night','twilight','vibrant_ink'
+			editor: null,
+			printMargin: true,
+			sessionLive: '',
+			sessionPreview: '',
+			theme: 'monokai',
+			themesBright: ['chrome', 'cloud9_day', 'eclipse', 'github', 'textmate'],
+			themesDark: [
+				'chaos', 'cloud9_night', 'github_dark', 'gruvbox', 'gruvbox_dark_hard',
+				'kr_theme', 'merbivore', 'mono_industrial', 'monokai', 'terminal',
+				'tomorrow_night', 'twilight', 'vibrant_ink'
 			],
-			wrap:false
+			wrap: false
 		};
 	},
-	emits:['clicked','update:modelValue'],
-	watch:{
+	emits: ['clicked', 'update:modelValue'],
+	watch: {
 		modelValue(v) {
-			if(v !== this.sessionLive.getValue()) {
+			if (this.editor !== null && v !== this.sessionLive.getValue()) {
 				this.sessionLive.setValue(v);
 				this.editor.clearSelection();
 			}
 		},
 		modelValueAlt(v) {
-			if(v === '') {
+			if (this.editor === null)
+				return;
+
+			if (v === "") {
 				this.sessionLive.setScrollTop(this.sessionPreview.getScrollTop());
 				this.editor.setSession(this.sessionLive);
 				return;
@@ -71,49 +72,52 @@ export default {
 			this.editor.clearSelection();
 		}
 	},
-	computed:{
-		capGen:  (s) => s.$store.getters.captions.generic,
-		settings:(s) => s.$store.getters.settings
+	computed: {
+		capGen: s => s.$store.getters.captions.generic,
+		settings: s => s.$store.getters.settings
 	},
 	mounted() {
-		// init ACE code editor
-		ace.require('ace/ext/searchbox');
-		this.sessionLive    = ace.createEditSession(this.modelValue);
-		this.sessionPreview = ace.createEditSession('');
+		jsLibraryLoadNoCache('externals/ace/ace.js').then(() => {
+			// init ACE code editor
+			ace.require('ace/ext/searchbox');
+			this.sessionLive = ace.createEditSession(this.modelValue);
+			this.sessionPreview = ace.createEditSession('');
 
-		// disable features like JS syntax checker
-		this.sessionLive.setUseSoftTabs(false);
-		this.sessionLive.setUseWorker(false);
-		this.sessionPreview.setUseWorker(false);
+			// disable features like JS syntax checker
+			this.sessionLive.setUseSoftTabs(false);
+			this.sessionLive.setUseWorker(false);
+			this.sessionPreview.setUseWorker(false);
 
-		this.editor = ace.edit(this.$refs.codeEditor);
-		this.editor.setSession(this.sessionLive);
-		this.editor.clearSelection();             // reset selection
-		this.editor.setHighlightActiveLine(true); // mark selected line
-		this.editor.on('change',this.change);
-		this.editor.on('click',this.click);
-		this.theme = this.builderOptionGet('codeEditorTheme',this.settings.dark ? 'cloud9_night' : 'cloud9_day');
-		this.wrap  = this.builderOptionGet('codeEditorWrap',false);
-		this.setOptions();
+			this.editor = ace.edit(this.$refs.codeEditor);
+			this.editor.setSession(this.sessionLive);
+			this.editor.clearSelection(); // reset selection
+			this.editor.setHighlightActiveLine(true); // mark selected line
+			this.editor.on('change', this.change);
+			this.editor.on('click', this.click);
+			this.theme = this.builderOptionGet('codeEditorTheme', this.settings.dark ? 'cloud9_night' : 'cloud9_day',);
+			this.wrap = this.builderOptionGet('codeEditorWrap', false);
+			this.setOptions();
 
-		// react to setting changes
-		this.$watch(() => [this.printMargin,this.readonly,this.settings,this.theme,this.wrap],this.setOptions);
+			// react to setting changes
+			this.$watch(() => [this.printMargin, this.readonly, this.settings, this.theme, this.wrap], this.setOptions);
+		}, this.$root.genericError);
 	},
 	unmounted() {
-		this.editor.destroy();
+		if (this.editor !== null)
+			this.editor.destroy();
 	},
-	methods:{
+	methods: {
 		// externals
 		builderOptionGet,
 		builderOptionSet,
 
 		// actions
 		change() {
-			if(!this.readonly)
-				this.$emit('update:modelValue',this.sessionLive.getValue());
+			if (!this.readonly)
+				this.$emit('update:modelValue', this.sessionLive.getValue());
 		},
 		click() {
-			if(!this.readonly && this.insertEntity !== null){
+			if (!this.readonly && this.insertEntity !== null) {
 				this.editor.insert(this.insertEntity);
 				this.$emit('clicked');
 			}
@@ -122,12 +126,12 @@ export default {
 		// editor functions
 		setOptions() {
 			this.editor.setOptions({
-				enableAutoIndent:true,
-				fontSize:17 * this.settings.fontSize / 100,
-				mode:`ace/mode/${this.mode}`,
-				readOnly:this.readonly,
-				theme:`ace/theme/${this.theme}`,
-				wrap:this.wrap
+				enableAutoIndent: true,
+				fontSize: 17 * this.settings.fontSize / 100,
+				mode: `ace/mode/${this.mode}`,
+				readOnly: this.readonly,
+				theme: `ace/theme/${this.theme}`,
+				wrap: this.wrap
 			});
 			this.editor.setShowPrintMargin(this.printMargin);
 		}
