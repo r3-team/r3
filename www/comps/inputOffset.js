39cfc6a120b34c05fb084cd44a85b10bcc466da1
Gabriel Herbert <gabriel@rei3.de>
2026-08-12 12:08:27 +0200

    Fix: Invalid page in offset selector if result count matches page limit for last page

--- www/comps/inputOffset.js
+++ www/comps/inputOffset.js
@@ -1,6 +1,6 @@
 export default {
-	name:'my-input-offset',
-	template:`<div class="input-offset default-inputs">
+	name: 'my-input-offset',
+	template: `<div class="my-input-offset default-inputs">
 		<!-- prev page -->
 		<my-button image="pagePrev.png"
 			v-if="arrows && limit < total"
@@ -8,24 +8,22 @@ export default {
 			:active="offset !== 0"
 			:naked="true"
 		/>
-		
+
 		<!-- offset selector -->
-		<select class="input-offset-selector"
-			v-if="offsetSelectShow" 
+		<select class="auto"
+			v-if="offsetSelectShow"
 			v-model="offsetSelect"
 			:class="classInput"
 			:title="captionText"
 		>
-			<option v-for="n in pages" :value="(n-1)*limit" :key="n">
+			<option v-for="n in pages" :class="{ currentPage:n === pageCurr }" :key="n" :value="(n-1)*limit">
 				{{ displayOffset(n) }}
 			</option>
 		</select>
-		
+
 		<!-- optional caption -->
-		<div v-if="caption" class="input-offset-caption">
-			{{ captionText }}
-		</div>
-		
+		<div v-if="caption" class="my-input-offset-caption">{{ captionText }}</div>
+
 		<!-- next page -->
 		<my-button image="pageNext.png"
 			v-if="arrows && limit < total"
@@ -34,68 +32,52 @@ export default {
 			:naked="true"
 		/>
 	</div>`,
-	props:{
-		arrows:    { type:Boolean, required:false, default:true },
-		caption:   { type:Boolean, required:false, default:false },
-		classInput:{ type:String,  required:false, default: '' },
-		limit:     { type:Number,  required:true },
-		offset:    { type:Number,  required:true },
-		total:     { type:Number,  required:true }
+	props: {
+		arrows: { type: Boolean, required: false, default: true },
+		caption: { type: Boolean, required: false, default: false },
+		classInput: { type: String, required: false, default: '' },
+		limit: { type: Number, required: true },
+		offset: { type: Number, required: true },
+		total: { type: Number, required: true }
 	},
-	emits:['input'],
-	computed:{
-		captionText:(s) => s.offsetSelectShow
-			? s.capGen.resultsOf.replace('{CNT}',s.total)
-			: s.capGen.results.replace('{CNT}',s.total),
-		offsetSelectShow:(s) => s.total > s.limit || s.offset !== 0,
-		pageCurr:(s) => parseInt(Math.ceil((s.offset+1) / s.limit)),
-		pageLast:(s) => parseInt(Math.ceil((s.total+1)  / s.limit)),
-		pages:(s) => {
-			if(s.total === 0 || s.limit === 0)
+	emits: ['input'],
+	computed: {
+		captionText: s => s.offsetSelectShow ? s.capGen.resultsOf.replace('{CNT}', s.total) : s.capGen.results.replace('{CNT}', s.total),
+		offsetSelectShow: s => s.total > s.limit || s.offset !== 0,
+		pageCurr: s => parseInt(Math.ceil((s.offset + 1) / s.limit), 10),
+		pageLast: s => parseInt(Math.ceil((s.total) / s.limit), 10),
+		pages: s => {
+			if (s.total === 0 || s.limit === 0)
 				return [];
-			
-			let pagesCount = 21;
-			let pageStart  = s.pageCurr;
-			let pageEnd    = s.pageCurr;
 
-			// add/substract at most pagesCount from current page, resulting in start & end page numbers
-			do {
-				if(pageStart > 1) {
-					pageStart--;
-					pagesCount--;
+			// show up to 20 pages, starting at most 10 pages before current one
+			const pages = [];
+			for (let page = s.pageCurr - 10; page <= s.pageLast && pages.length <= 20; page++) {
+				if (page >= 1) {
+					pages.push(page);
 				}
-				if(pageEnd < s.pageLast) {
-					pageEnd++;
-					pagesCount--;
-				}
-			} while(pagesCount > 1 && (pageStart > 1 || pageEnd < s.pageLast));
-
-			// generate page list for the selector
-			let pages = [];
-			for(let i = pageStart; i <= pageEnd; i++) {
-				pages.push(i)
 			}
 			return pages;
 		},
-		
+
 		// inputs
-		offsetSelect:{
-			get()  { return this.offset; },
-			set(v) { this.$emit('input',v); }
+		offsetSelect: {
+			get() { return this.offset; },
+			set(v) { this.$emit('input', v); }
 		},
-		
+
 		// stores
-		capGen:(s) => s.$store.getters.captions.generic
+		capGen: s => s.$store.getters.captions.generic
 	},
-	methods:{
+	methods: {
 		displayOffset(page) {
 			return page === this.pageLast
-				? ((page-1)*this.limit)+1 + ' - ' + this.total
-				: ((page-1)*this.limit)+1 + ' - ' + (((page-1)*this.limit) + this.limit);
+				? `${((page - 1) * this.limit) + 1} - ${this.total}`
+				: `${((page - 1) * this.limit) + 1} - ${((page - 1) * this.limit) + this.limit}`;
 		},
 		pageChanged(next) {
-			if(next) this.$emit('input',this.offset + this.limit);
-			else     this.$emit('input',this.offset - this.limit);
+			if (next) this.$emit('input', this.offset + this.limit);
+			else this.$emit('input', this.offset - this.limit);
 		}
 	}
 };
